3afe04f9e6a99e2331f40b09f1b1a520bd390dd3
Sven Clement <sven.clement@iwi.dfki.de>
2013-02-16 04:14:23 +0100

    added graph to the public view

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -21,5 +21,13 @@ class StaticController < ApplicationController
   
   def public
     @website = Website.find_by_public_token(params[:id])
+    @stats = Hash.new
+    @website.statistics.each do |s|
+      if @stats[s.fetch_date].nil? 
+        @stats[s.fetch_date] = {:facebook => s.facebook, :twitter => s.twitter, :gplus => s.gplus, :linkedin => s.linkedin}
+      else
+        @stats[s.fetch_date] = {:facebook => @stats[s.fetch_date][:facebook] + s.facebook, :twitter => @stats[s.fetch_date][:twitter] + s.twitter, :gplus => @stats[s.fetch_date][:gplus] + s.gplus, :linkedin => @stats[s.fetch_date][:linkedin] + s.linkedin}
+      end
+    end
   end
 end
